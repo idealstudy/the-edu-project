7a7126986ad64baed2add6be2cf43f3d96136cc4
OHS <ohs6006@gmail.com>
2025-06-28 15:42:57 +0900

    chore : 코드 정리

--- src/app/(dashboard)/dashboard/logs/write/page.tsx
+++ src/app/(dashboard)/dashboard/logs/write/page.tsx
@@ -1,9 +1,12 @@
+import LogWriteForm from '@/features/dashboard/log/components/log-write-form';
+
 export default function LogWritePage() {
   return (
-    <main className="mx-auto flex max-w-[807px] items-center justify-center py-[92px]">
+    <main className="mx-auto flex max-w-[807px] flex-col items-center justify-center py-[92px]">
       <h1 className="text-center text-[32px] font-bold">
         수업에 대해 작성해 주세요.
       </h1>
+      <LogWriteForm />
     </main>
   );
 }
