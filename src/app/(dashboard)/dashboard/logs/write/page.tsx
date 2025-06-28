702b73b773b8651128516f3d71ccf3f4e577386d
OHS <ohs6006@gmail.com>
2025-06-28 12:36:51 +0900

    feat : 로그페이지 UI

---
+++ src/app/(dashboard)/dashboard/logs/write/page.tsx
@@ -0,0 +1,9 @@
+export default function LogWritePage() {
+  return (
+    <main className="mx-auto flex max-w-[807px] items-center justify-center py-[92px]">
+      <h1 className="text-center text-[32px] font-bold">
+        수업에 대해 작성해 주세요.
+      </h1>
+    </main>
+  );
+}
---
+++ src/features/dashboard/log/components/log-write-form.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LogWriteForm = () => {
+  return <div className="">LogWriteForm</div>;
+};
+
+export default LogWriteForm;
