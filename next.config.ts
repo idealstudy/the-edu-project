dba1618e8fff76ac6573deec3e45f4e8432c6f48
JangHwan <wkd5878@gmail.com>
2025-11-10 14:16:26 +0900

    :bug: fix(BTS-300): svg webpack 수정

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,23 @@ const nextConfig: NextConfig = {
       },
     },
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
 };
 
 export default nextConfig;
