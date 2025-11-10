fdb7a3a143b42c465f5885bd9edbd3318b4cafce
JangHwan <wkd5878@gmail.com>
2025-11-11 07:04:18 +0900

    :sparkle: feat(BTS-301): 이미지 상수 파일 추가 및 로딩스피너 컴포넌트 생성

---
+++ src/components/loading/full-screen-loader.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const FullScreenLoader = () => {
+  return <div></div>;
+};
---
+++ src/constants/images.ts
@@ -0,0 +1,7 @@
+export const IMG = {
+  CHARACTER: {
+    TYPE01: '/character/img_signup_type01.png',
+    TYPE02: '/character/img_signup_type02.png',
+    TYPE03: '/character/img_signup_type03.png',
+  },
+} as const;
