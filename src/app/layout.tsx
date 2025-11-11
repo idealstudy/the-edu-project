2a9cc02ad47d0af39db2cb7797baee2400bd558f
JangHwan <wkd5878@gmail.com>
2025-11-12 08:34:27 +0900

    :sparkle: feat(BTS-300): shared 폴더 생성(공용 로직 모음)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 
-import '@/components/editor/styles/text-editor.css';
+import '@/shared/components/editor/styles/text-editor.css';
 import { Header } from '@/shared/components/layout/header';
 import { GlobalProvider } from '@/shared/providers/global-provider';
 import '@/styles/globals.css';
