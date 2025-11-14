44fad38de07e3e5fd7e90efe84eb0090373612f1
JangHwan <wkd5878@gmail.com>
2025-11-14 09:01:08 +0900

    :recycle: refactor(BTS-300): sidebar 컴포넌트 재사용 가능한 컴포넌트로 분리(shared 레이어로 이동) 및 대시보드 전용 사이드바 컴포넌트 생성

--- src/app/(private)/study-rooms/new/page.tsx
+++ src/app/(private)/study-rooms/new/page.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 import CreateStudyRoomFlow from '@/features/study-rooms/components/create/CreateStudyRoomFlow';
 import { ColumnLayout } from '@/layout/column-layout';
-import { Sidebar } from '@/layout/sidebar';
 
 export default function CreateStudyRoomPage() {
   return (
     <div className="desktop:pl-sidebar-width flex flex-col bg-[#F9F9F9]">
-      <Sidebar />
+      {/*<Sidebar />*/}
       <ColumnLayout className="container mx-auto">
         <CreateStudyRoomFlow />
       </ColumnLayout>
