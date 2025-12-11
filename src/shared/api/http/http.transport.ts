168ce0635740e809a1a47396d16d236d239b1936
시원 <sw622@naver.com>
2025-12-11 13:52:06 +0900

    refactor: private HTTP 클라이언트 환경별 baseURL 분기 설정 - 개발 환경: BFF 경유 (/api/v1) - 프로덕션 환경: 백엔드 직접 통신

--- src/shared/api/http/http.transport.ts
+++ src/shared/api/http/http.transport.ts
@@ -1,7 +1,7 @@
 import { env } from '@/shared/constants/api';
 import axios from 'axios';
 
-const isServer = typeof window === 'undefined';
+// const isServer = typeof window === 'undefined';
 
 // NOTE: 인증 필요한 요청 전용 (쿠키 자동 포함)
 const privateHttp = axios.create({
@@ -10,7 +10,9 @@ const privateHttp = axios.create({
    * 따라서 /api/v1 경로로 요청하도록 설정하며 app/v1/[...path] 라우터 핸들러를 통해 쿠키를 붙여 백엔드로 프록시
    * ────────────────────────────────────────────────────*/
   // baseURL: env.backendApiUrl,
-  baseURL: isServer ? env.backendApiUrl : '/api/v1',
+  // baseURL: isServer ? env.backendApiUrl : '/api/v1',
+  baseURL:
+    process.env.NODE_ENV === 'development' ? '/api/v1' : env.backendApiUrl,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
