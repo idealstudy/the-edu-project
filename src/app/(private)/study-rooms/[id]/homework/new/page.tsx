0c12730a1d168b7ba0bf5399eca6020aa83db0c1
jihwan2001 <coghtoy@naver.com>
2026-01-09 17:26:33 +0900

    refactor: 컴포넌트 명 변경

--- src/app/(private)/study-rooms/[id]/homework/new/page.tsx
+++ src/app/(private)/study-rooms/[id]/homework/new/page.tsx
@@ -1,6 +1,6 @@
 import BackLink from '@/features/dashboard/studynote/components/back-link';
 import SelectArea from '@/features/dashboard/studynote/write/components/select-area';
-import { HomeworkFormProvider } from '@/features/homework/components/write/components/form-provider';
+import { HomeworkFormProvider } from '@/features/homework/components/write/components/homework-form-provider';
 import { WriteArea } from '@/features/homework/components/write/components/write-area';
 import { HomeworkWriteForm } from '@/features/homework/components/write/components/write-form';
 import { ColumnLayout } from '@/layout/column-layout';
