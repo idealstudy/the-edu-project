11ef69c1875246be988d74ffceae82e4e5ca0049
시원 <sw622@naver.com>
2026-02-10 23:31:13 +0900

    feat: 프로필 임시 처리 (API 변경 예정) - 이름, 이메일 실제 데이터 제공 - 공개 여부, 역할, 한 줄 소개 등 임시 처리

--- src/features/profile/components/profile-main.tsx
+++ src/features/profile/components/profile-main.tsx
@@ -1,21 +1,28 @@
+'use client';
+
 import ProfileLayout from '@/features/profile/components/profile-layout';
+import { useProfile } from '@/features/profile/hooks/use-profile';
 
-const MOCK_PROFILE_DATA = {
-  name: 'string',
-  email: 'string',
-  isProfilePublic: true,
-  simpleIntroduction: 'string',
-  role: 'ROLE_TEACHER',
-  profilePublicKorean: '공개',
-};
-
-export default function ProfileMain({}: { userId: string }) {
+export default function ProfileMain({ userId }: { userId: string }) {
   // TODO 새 API 연결
-  const profileData = MOCK_PROFILE_DATA;
+  const { data: profileData, isLoading } = useProfile(userId);
+
+  const MOCK_PROFILE_DATA = {
+    name: profileData?.name || '',
+    email: profileData?.email || '',
+    isProfilePublic: true,
+    simpleIntroduction: null,
+    role: 'ROLE_TEACHER',
+    profilePublicKorean: '공개',
+  };
+
+  if (isLoading) {
+    return <div className="text-center">로딩중...</div>;
+  }
 
   if (!profileData) {
     return <div className="text-center">프로필 정보를 불러올 수 없습니다.</div>;
   }
 
-  return <ProfileLayout basicInfo={profileData} />;
+  return <ProfileLayout basicInfo={MOCK_PROFILE_DATA} />;
 }
