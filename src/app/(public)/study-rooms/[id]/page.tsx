95857ffa02bf02cfeadd8e1be3bc430bc155aa69
jihwan2001 <coghtoy@naver.com>
2026-02-14 23:35:25 +0900

    fix: 스터디룸 ui 수정

--- src/app/(public)/study-rooms/[id]/page.tsx
+++ src/app/(public)/study-rooms/[id]/page.tsx
@@ -9,7 +9,7 @@ export default function StudyRoomDetailPage() {
       <ColumnLayout.Left className="border-line-line1 flex h-fit flex-col gap-5 rounded-xl border bg-white px-8 py-8">
         <PublicStudyroomSidebar />
       </ColumnLayout.Left>
-      <ColumnLayout.Right className="desktop:max-w-[740px] desktop:px-8 flex flex-col gap-3 rounded-[12px]">
+      <ColumnLayout.Right className="desktop:max-w-[740px] desktop:px-8 flex flex-col gap-3 rounded-[12px] py-2">
         <div className="border-line-line1 flex flex-col gap-9 rounded-[12px] border bg-white py-2">
           <PublicStudyroomsContents />
         </div>
