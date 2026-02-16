47877bd8eca280406dad2b3ea2cb9bdbab70af2b
jihwan2001 <coghtoy@naver.com>
2026-02-16 18:02:48 +0900

    fix: 선생님 프로필페이지 스타일 수정

--- src/app/(home)/list/teachers/page.tsx
+++ src/app/(home)/list/teachers/page.tsx
@@ -39,7 +39,7 @@ export default function TeachersListPage() {
   }
 
   return (
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-4">
       {data?.content.map((teacher) => (
         <TeacherCard
           key={teacher.id}
