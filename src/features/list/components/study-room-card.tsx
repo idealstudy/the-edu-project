9724fd7d7683405fd92fb38d61b8c0373fecadec
jihwan2001 <coghtoy@naver.com>
2026-02-16 18:28:13 +0900

    fix: 스터디룸 페이지 스타일 수정

--- src/features/list/components/study-room-card.tsx
+++ src/features/list/components/study-room-card.tsx
@@ -20,21 +20,26 @@ export const StudyRoomCard = ({ studyRoom }: StudyRoomCardProps) => {
       )}
     >
       {/* 상단 영역 */}
-      <div className="bg-orange-scale-orange-1 flex flex-col gap-[10px] p-6">
-        <h3 className="font-body1-heading text-gray-scale-gray-95 line-clamp-1">
+      <div className="bg-orange-1 flex flex-col gap-[10px] p-6">
+        <h3 className="font-body1-heading text-gray-12 line-clamp-1">
           {studyRoom.name}
         </h3>
-        <p className="font-body2-normal text-gray-scale-gray-70 line-clamp-2 min-h-[50px]">
+        <p className="font-body2-normal text-gray-10 line-clamp-2 min-h-[66px]">
           {studyRoom.description}
         </p>
       </div>
 
       {/* 하단 영역 */}
       <div className="border-gray-scale-gray-10 flex flex-col gap-[8px] border-t-1 bg-white p-6">
-        <div className="font-label-heading bg-orange-scale-orange-5 text-orange-scale-orange-50 flex h-[24px] w-fit items-center justify-center rounded-[4px] px-[8px] text-[12px]">
-          영어
+        <div className="flex gap-2">
+          <div className="bg-orange-7 font-label-heading h-[27px] w-[47px] rounded-full px-2 py-1 text-white">
+            NEW
+          </div>
+          <div className="bg-orange-2 text-orange-7 font-label-heading flex w-fit items-center justify-center rounded-[4px] px-[8px] py-[4px]">
+            영어
+          </div>
         </div>
-        <div className="font-body1-heading text-gray-scale-gray-95 line-clamp-1">
+        <div className="font-body1-heading text-gray-12 truncate text-lg md:text-xl">
           {studyRoom.teacherName} 선생님
         </div>
       </div>
