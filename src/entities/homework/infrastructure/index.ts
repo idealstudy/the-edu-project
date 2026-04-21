92d1aa5bab227270f65d1d5a026cc79c8c57cb78
jihwan2001 <coghtoy@naver.com>
2026-04-22 01:12:10 +0900

    feat: homework 부분 부모 권한 설정

--- src/entities/homework/infrastructure/index.ts
+++ src/entities/homework/infrastructure/index.ts
@@ -2,6 +2,7 @@ export { dto } from './homework.dto';
 export { payload } from './homework.dto';
 export { repository } from './homework.repository';
 export {
+  ParentHomeworkQueryKey,
   StudentHomeworkQueryKey,
   TeacherHomeworkQueryKey,
 } from './homework.keys';
--- src/features/homework/components/detail/homework-detail-role-switch.tsx
+++ src/features/homework/components/detail/homework-detail-role-switch.tsx
@@ -2,26 +2,44 @@
 
 import { useReducer } from 'react';
 
+import { ParentHomeworkDetail } from '@/features/homework/components/detail/parent-homework-detail';
 import { StudentHomeworkDetail } from '@/features/homework/components/detail/student-homework-detail';
 import { TeacherHomeworkDetail } from '@/features/homework/components/detail/teacher-homework-detail';
 import { dialogReducer, initialDialogState } from '@/shared/components/dialog';
+import { MiniSpinner } from '@/shared/components/loading';
 import { useRole } from '@/shared/hooks';
 
 type Props = {
   studyRoomId: number;
   homeworkId: number;
+  studentId: number;
 };
 
 export default function HomeworkDetailRoleSwitch({
   studyRoomId,
   homeworkId,
+  studentId,
 }: Props) {
-  const { role } = useRole();
+  const { role, isLoading: isRoleLoading } = useRole();
   const [dialog, dispatch] = useReducer(dialogReducer, initialDialogState);
+
+  if (isRoleLoading) return <MiniSpinner />;
 
   if (role === 'ROLE_TEACHER') {
     return (
       <TeacherHomeworkDetail
+        studyRoomId={studyRoomId}
+        homeworkId={homeworkId}
+        dialog={dialog}
+        dispatch={dispatch}
+      />
+    );
+  }
+
+  if (role === 'ROLE_PARENT') {
+    return (
+      <ParentHomeworkDetail
+        studentId={studentId}
         studyRoomId={studyRoomId}
         homeworkId={homeworkId}
         dialog={dialog}
